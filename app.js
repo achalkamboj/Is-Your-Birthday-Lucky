0fa158fcaeb1bb93a109db91c6e124054c5994b1
Achal-Kamboj <ak6861@srmist.edu.in>
2021-08-20 22:36:30 +0530

    final commit

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkButton = document.querySelector("#check-number");
+const outputBox = document.querySelector("#output-box");
 
-checkButton.addEventListener('click' , function getvalues(){
-    console.log(dateOfBirth.value, luckyNumber.value);
-})
+function checkLuck(){
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+    if(sum&&dob){
+    if(sum%luckyNumber.value===0)
+        outputBox.innerText="Congratulations...Your Birthday Is Lucky!";
+    else
+        outputBox.innerText="Luck is just a parameter to measure hard work!";
+    }
+    else{
+        outputBox.innerText="Please enter both the fields";
+    }
+}
+
+function calculateSum(dob){
+    dob = dob.replaceAll("-","");
+    let sum=0;
+    for(var i=0;i<dob.length;i++){
+        sum = sum + Number(dob.charAt(i));
+    }
+    return sum;
+}
+
+checkButton.addEventListener('click' , checkLuck)
+
+    
+    
+
